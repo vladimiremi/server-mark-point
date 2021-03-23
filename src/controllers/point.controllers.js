15e62ef112ced086ac1b4e7f481909f604d2d68c
vladimiremi <vladimiremi2014@gmail.com>
2021-03-23 19:34:01 -0300

    chore: add point registration rules and timezone moment release

--- src/controllers/point.controllers.js
+++ src/controllers/point.controllers.js
@@ -1,13 +1,58 @@
 const Point = require('../models/Point');
+const moment = require('moment');
+const Sequelize = require('sequelize');
+
+
 
 /**registrar ponto**/
 const postPoint = async ( request, response ) => {
 
+    const {latitude, longitude} = request.body;
     const id_collaborator = request.headers.authorization;
+    const timestring = moment().format('YYYY-MM-DD[T]HH:mm:ss');
+    
+    const Op = Sequelize.Op;              // biblioteca de operadores
+    const query = `${timestring.slice(0, 10)}%`; // string de consulta
+    console.log(query)
 
-    const point = await Point.create({ id_collaborator });
+    //busca pontos de um mesmo dia
+    const daypoints = await Point.findAll({ 
+        where: { 
+            timestring: { [Op.like]: query },
+            id_collaborator: id_collaborator
+        } 
+    });
+    
+    //só permite cadastrar 4 pontos em um dia.
+    if (daypoints < 4) {
+        //cria o ponto
+        const point = await Point.create({ 
+            id_collaborator, 
+            latitude, longitude, 
+            timestring
+        });
 
-    return response.json(point);
+         // retorna o ponto que foi criado
+        const seePoint = await Point.findAll({
+            where: {
+                id: point.id
+            }
+        });
+
+        return response.json(seePoint);
+    } else {
+        response.json("Já foram cadastrado 4 pontos, no dia de hoje.");
+    }
+
+
+    
+       
+
+   
+
+    // console.log(point);
+    console.log(daypoints.length)
+
 }
 
 //listar os pontos do colaborador
--- src/models/Point.js
+++ src/models/Point.js
@@ -3,6 +3,9 @@ const { Model, DataTypes } = require('sequelize');
 class Point extends Model {
     static init(sequelize) {
         super.init({
+            timestring: DataTypes.STRING,
+            latitude: DataTypes.STRING,
+            longitude: DataTypes.STRING,
             id_collaborator: DataTypes.STRING
         }, {
             sequelize,
